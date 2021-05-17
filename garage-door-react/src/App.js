1db0d112b408618d2272a1a3b7fa9b852d7791f3
ALearningCurve <willwallsoto@gmail.com>
2021-05-17 12:19:53 -0400

    notification progress

--- garage-door-react/src/App.js
+++ garage-door-react/src/App.js
@@ -3,29 +3,103 @@ import DoorsView from './components/DoorsView';
 import LoginHandler from "./components/LoginHandler";
 import firebase from './firebase.js'; 
 import React, { Component } from 'react'
-import { getToken, onMessageListener } from './firebase';
 import {Button, Row, Col, Toast} from 'react-bootstrap';
+const { toast, snackbar } = require('tailwind-toast')
 
 export default class App extends Component {
-  db; auth;
+  db; auth; messaging;
   
   constructor(props) {
     super(props)
   
     this.state = {
-        user: null
+        user: null,
+        message: null,
+        token:false
     }
 
+    this.initFirestore();
+  }
+
+  initFirestore = () => {
     this.db = firebase.firestore();
     this.db.settings({
         timestampsInSnapshots: true
     });
     
     this.auth = firebase.auth()
-    
-    // this.auth.useEmulator('http://localhost:9099/', { disableWarnings: true });
-    // this.db.useEmulator('localhost', 8080);
-    
+    this.messaging = firebase.messaging();
+
+    this.initPushNotifcations();
+  }
+
+  getToken = (setTokenFound) => {
+    return this.messaging.getToken({vapidKey: 'GENERATED_MESSAGING_KEY'}).then((currentToken) => {
+      if (currentToken) {
+        console.log('current token for client: ', currentToken);
+        setTokenFound(true);
+        // Track the token -> client mapping, by sending to backend server
+        // show on the UI that permission is secured
+      } else {
+        console.log('No registration token available. Request permission to generate one.');
+        setTokenFound(false);
+        // shows on the UI that permission is required 
+      }
+    }).catch((err) => {
+      console.log('An error occurred while retrieving token. ', err);
+      // catch error while creating client token
+    });
+  }
+  onMessageListener = () =>
+    new Promise((resolve) => {
+      this.messaging.onMessage((payload) => {
+        resolve(payload);
+      });
+  });
+
+  initPushNotifcations = () => {
+    this.getToken(()=>{this.setState({token:true})});
+
+    this.onMessageListener()
+      .then(message => {
+        this.setState({
+          message:true
+        })
+      .catch(err => {
+        alert("failed: " + err)
+      })
+    })
+  }
+
+  createMessage = () => {
+    this.setState({
+      message:true
+    })
+    toast().default('Title', 'Message!').show()
+  }
+
+  renderPushNotifaction = () => {
+    return (
+      <div>
+        {/* <Toast onClose={() => this.setState({message:null}) } show={this.state.message} delay={3000} autohide animation style={{
+          position: 'absolute',
+          top: 20,
+          right: 20,
+        }}>
+          <Toast.Header>
+            <img
+              src="holder.js/20x20?text=%20"
+              className="rounded mr-2"
+              alt=""
+            />
+            <strong className="mr-auto">Notification</strong>
+            <small>12 mins ago</small>
+          </Toast.Header>
+          <Toast.Body>There are some new updates that you might love!</Toast.Body>
+        </Toast> */}
+        <Button onClick={() => this.createMessage()}>Show Toast</Button>
+      </div>
+    )
   }
   
   componentDidMount() {
@@ -52,6 +126,7 @@ export default class App extends Component {
           <DoorsView db={db} user={user}/>
         }
         <LoginHandler auth={this.auth}/>
+        {this.renderPushNotifaction()}
       </div>
       </div>
     );
